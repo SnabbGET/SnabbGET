da7bf66d62af8cab35cb582e3e0e422989fbc20c
LAPCoder <97749743+LAPCoder@users.noreply.github.com>
2023-12-26 16:24:45 +0100

    A third MAJOR changement

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,78 @@
-// Copyright 2020 Arthur Sonzogni. All rights reserved.
+// Copyright 2022 Arthur Sonzogni. All rights reserved.
 // Use of this source code is governed by the MIT license that can be found in
 // the LICENSE file.
 #include <chrono>                  // for operator""s, chrono_literals
-#include <cmath>                   // for sin
-#include <ftxui/dom/elements.hpp>  // for graph, operator|, separator, color, Element, vbox, flex, inverted, operator|=, Fit, hbox, size, border, GREATER_THAN, HEIGHT
-#include <ftxui/screen/screen.hpp>  // for Full, Screen
-#include <functional>               // for ref, reference_wrapper
-#include <iostream>                 // for cout, ostream
-#include <memory>                   // for shared_ptr
-#include <string>                   // for operator<<, string
-#include <thread>                   // for sleep_for
-#include <vector>                   // for vector
+#include <ftxui/dom/elements.hpp>  // for filler, operator|, separator, text, border, Element, vbox, vtext, hbox, center, gaugeDown, gaugeLeft, gaugeRight, gaugeUp
+#include <ftxui/screen/screen.hpp>  // for Screen
+#include <iostream>                 // for cout, endl, ostream
+#include <string>  // for allocator, operator+, operator<<, string, to_string
+#include <thread>  // for sleep_for
  
-#include "ftxui/dom/node.hpp"  // for Render
-#include "ftxui/screen/color.hpp"  // for Color, Color::BlueLight, Color::RedLight, Color::YellowLight, ftxui
- 
-class Graph {
- public:
-  std::vector<int> operator()(int width, int height) const {
-    std::vector<int> output(width);
-    for (int i = 0; i < width; ++i) {
-      float v = 0;
-      v += 0.1f * sin((i + shift) * 0.1f);        // NOLINT
-      v += 0.2f * sin((i + shift + 10) * 0.15f);  // NOLINT
-      v += 0.1f * sin((i + shift) * 0.03f);       // NOLINT
-      v *= height;                                // NOLINT
-      v += 0.5f * height;                         // NOLINT
-      output[i] = static_cast<int>(v);
-    }
-    return output;
-  }
-  int shift = 0;
-};
- 
-std::vector<int> triangle(int width, int height) {
-  std::vector<int> output(width);
-  for (int i = 0; i < width; ++i) {
-    output[i] = i % (height - 4) + 2;
-  }
-  return output;
-}
+#include "ftxui/dom/node.hpp"      // for Render
+#include "ftxui/screen/color.hpp"  // for ftxui
  
 int main() {
   using namespace ftxui;
   using namespace std::chrono_literals;
  
-  Graph my_graph;
- 
   std::string reset_position;
-  for (int i = 0;; ++i) {
+  for (float percentage = 0.0f; percentage <= 1.0f; percentage += 0.002f) {
+    std::string data_downloaded =
+        std::to_string(int(percentage * 5000)) + "/5000";
+ 
+    auto gauge_up =  //
+        hbox({
+            vtext("gauge vertical"),
+            separator(),
+            gaugeUp(percentage),
+        }) |
+        border;
+ 
+    auto gauge_down =  //
+        hbox({
+            vtext("gauge vertical"),
+            separator(),
+            gaugeDown(percentage),
+        }) |
+        border;
+ 
+    auto gauge_right =  //
+        vbox({
+            text("gauge horizontal"),
+            separator(),
+            gaugeRight(percentage),
+        }) |
+        border;
+ 
+    auto gauge_left =  //
+        vbox({
+            text("gauge horizontal"),
+            separator(),
+            gaugeLeft(percentage),
+        }) |
+        border;
+ 
     auto document = hbox({
+        gauge_up,
+        filler(),
         vbox({
-            graph(std::ref(my_graph)),
-            separator(),
-            graph(triangle) | inverted,
-        }) | flex,
-        separator(),
-        vbox({
-            graph(std::ref(my_graph)) | color(Color::BlueLight),
-            separator(),
-            graph(std::ref(my_graph)) | color(Color::RedLight),
-            separator(),
-            graph(std::ref(my_graph)) | color(Color::YellowLight),
-        }) | flex,
+            gauge_right,
+            filler(),
+            text(data_downloaded) | border | center,
+            filler(),
+            gauge_left,
+        }),
+        filler(),
+        gauge_down,
     });
  
-    document |= border;
- 
-    const int min_width = 40;
-    document |= size(HEIGHT, GREATER_THAN, min_width);
- 
-    auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+    auto screen = Screen(32, 16);
     Render(screen, document);
     std::cout << reset_position;
     screen.Print();
     reset_position = screen.ResetPosition();
  
-    const auto sleep_time = 0.03s;
-    std::this_thread::sleep_for(sleep_time);
-    my_graph.shift++;
+    std::this_thread::sleep_for(0.01s);
   }
- 
-  return 0;
+  std::cout << std::endl;
 }
